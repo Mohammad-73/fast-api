651780e4518632cc196335ac2459a76db060686c
khalili <m.khalili7310@gmail.com>
2025-05-14 11:40:08 +0330

    json formatted log file

--- first-project-with-fast-api/storeapi/logging_conf.py
+++ first-project-with-fast-api/storeapi/logging_conf.py
@@ -2,21 +2,29 @@ from logging.config import dictConfig
 
 from storeapi.config import DevConfig,config
 
+filter = asgi_correlation_id.CorrelationIdFilter(uuid_length=8, default_value="-")
 def  configure_logging()->None:
     dictConfig(
         {
             "version":1,
             "disable_existing_loggers":False,
+            "filters":{
+                "correlation_id":{
+                    "()": "asgi_correlation_id.CorrelationIdFilter",
+                    "uuid_length": 8 if isinstance(config,DevConfig) else 32,
+                    "default_value": "-"
+                }
+            },
             "formatters":{
                 "console":{
                     "class": "logging.Formatter",
                     "datefmt":"%Y-%m-%dT%H:%M:%S",
-                    "format":"%(name)s:%(lineno)d - %(message)s"
+                    "format":"(%(correlation_id)s) %(name)s:%(lineno)d - %(message)s"
                 },
                 "file":{
-                     "class": "logging.Formatter",
+                     "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
                     "datefmt":"%Y-%m-%dT%H:%M:%S",
-                    "format":"%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s"
+                    "format":"%(asctime)s %(msecs)03d %(levelname)-8s %(correlation_id)s %(name)s %(lineno)d %(message)s"
                 }
             },
             "handlers":{
