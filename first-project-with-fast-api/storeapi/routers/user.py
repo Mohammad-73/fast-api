e4ca3f2dec91fb50d5a5e144c7261536fbe68894
khalili <m.khalili7310@gmail.com>
2025-05-17 11:52:10 +0330

    hash password

--- first-project-with-fast-api/storeapi/routers/user.py
+++ first-project-with-fast-api/storeapi/routers/user.py
@@ -2,7 +2,7 @@ import logging
 
 from fastapi import APIRouter, HTTPException, status
 from storeapi.models.user import UserIn
-from storeapi.security import get_user
+from storeapi.security import get_password_hash,get_user
 from storeapi.database import database, user_table
 
 logger = logging.getLogger(__name__)
@@ -15,7 +15,8 @@ async def register(user: UserIn):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="A user with that email already exist"
         )
-    query = user_table.insert().values(email=user.email, password=user.password)
+    hashed_password = get_password_hash(user.password)
+    query = user_table.insert().values(email=user.email, password=hashed_password)
 
     logger.debug(query)
 
