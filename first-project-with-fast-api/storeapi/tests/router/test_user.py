049930ac9f9da53565caaee80b488cbd145726e0
khalili <m.khalili7310@gmail.com>
2025-05-18 11:22:26 +0330

    generate the access token

--- first-project-with-fast-api/storeapi/tests/router/test_user.py
+++ first-project-with-fast-api/storeapi/tests/router/test_user.py
@@ -1,10 +1,12 @@
 import pytest
 from httpx import AsyncClient
 
-async def register_user(async_client: AsyncClient,email: str, password: str):
+
+async def register_user(async_client: AsyncClient, email: str, password: str):
     return await async_client.post(
-        "/register", json={"email":email, "password":password}
+        "/register", json={"email": email, "password": password}
     )
+
 
 @pytest.mark.anyio
 async def test_register_user(async_client: AsyncClient):
@@ -12,8 +14,33 @@ async def test_register_user(async_client: AsyncClient):
     assert response.status_code == 201
     assert "User created" in response.json()["detail"]
 
+
 @pytest.mark.anyio
-async def test_register_user_already_exist(async_client: AsyncClient, register_user: dict):
-    response = await register_user(async_client, register_user["email"], register_user["password"])
+async def test_register_user_already_exist(
+    async_client: AsyncClient, register_user: dict
+):
+    response = await register_user(
+        async_client, register_user["email"], register_user["password"]
+    )
     assert response.status_code == 400
     assert "already exists" in response.json()["detail"]
+
+
+@pytest.mark.anyio
+async def test_login_user_not_exist(async_client: AsyncClient):
+    response = await async_client.post(
+        "/token", json={"email": "test@example.net", "password": "1234"}
+    )
+    assert response.status_code == 401
+
+
+@pytest.mark.anyio
+async def test_login_user(async_client: AsyncClient, registered_user: dict):
+    response = await async_client.post(
+        "/token",
+        json={
+            "email": registered_user["email"],
+            "password": registered_user["password"],
+        },
+    )
+    assert response.status_code == 200
