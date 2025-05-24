1e62e4b559b12c32ccbf66af3937d88d77adb796
khalili <m.khalili7310@gmail.com>
2025-05-24 08:52:38 +0330

    query string arguments and data sorting with Enum

--- first-project-with-fast-api/storeapi/routers/post.py
+++ first-project-with-fast-api/storeapi/routers/post.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum
 from typing import Annotated
 
 import sqlalchemy
@@ -13,6 +14,7 @@ from storeapi.models.post import (
     UserPost,
     UserPostIn,
     UserPostWithComments,
+    UserPostWithLikes,
 )
 from storeapi.models.user import User
 from storeapi.security import get_current_user
@@ -52,9 +54,24 @@ async def create_post(
     return {**data, "id": last_record_id}
 
 
-@router.get("/post", response_model=list[UserPost])
-async def get_all_posts():
-    query = post_table.select()
+class PostSorting(str, Enum):
+    new = "new"
+    old = "old"
+    most_likes = "most_likes"
+
+
+@router.get("/post", response_model=list[UserPostWithLikes])
+async def get_all_posts(sorting: PostSorting = PostSorting.new):
+    logger.info("Getting all posts")
+
+    if sorting == PostSorting.new:
+        query = select_post_and_likes.order_by(post_table.c.id.desc())
+    elif sorting == PostSorting.old:
+        query = select_post_and_likes.order_by(post_table.c.id.asc())
+    elif sorting == PostSorting.most_likes:
+        query = select_post_and_likes.order_by(
+            sqlalchemy.desc("likes")
+        )  # http://api.com/post?sorting=most_likes
 
     logger.debug(query)
 
